b22edc1cee3c1b6d5ce76f8f29445bc3726bc588
Bram Klapwijk <bram.klapwijk00@outlook.com>
2020-02-05 10:35:30 +0100

    add functions

--- calculator/src/test/java/org/oose/dea/CalculatorTest.java
+++ calculator/src/test/java/org/oose/dea/CalculatorTest.java
@@ -8,7 +8,28 @@ public class CalculatorTest {
     @Test
     public void testAdd(){
         Calculator calculator = new Calculator ();
-        assertEquals(4,calculator.add(2,2));
-        assertEquals(100,calculator.add(25,75));
+        assertEquals(4, calculator.add(2,2));
+        assertEquals(100, calculator.add(25,75));
+    }
+
+    @Test
+    public void testSubtract(){
+        Calculator calculator = new Calculator ();
+        assertEquals(-4, calculator.subtract(8,12));
+        assertEquals(100, calculator.subtract(150,50));
+    }
+
+    @Test
+    public void testMultiply(){
+        Calculator calculator = new Calculator ();
+        assertEquals(4, calculator.multiply(2,2));
+        assertEquals(100, calculator.multiply(2,50));
+    }
+
+    @Test
+    public void testDivide(){
+        Calculator calculator = new Calculator ();
+        assertEquals(1, calculator.divide(2,2));
+        assertEquals(5, calculator.divide(50,10));
     }
 }
